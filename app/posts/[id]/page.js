40e0244c08a6b2764843d0f2cc76d8209054cd3c
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-18 17:41:41 +0200

    Implement static site generation for individual blogs

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,10 +1,16 @@
-import { getPost } from "@actions/posts";
+import { getPost, getPosts } from "@actions/posts";
+import Comments from "@components/posts/comments/Comments";
 import formatDate from "@utils/formatDate";
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import css from "./page.module.css";
 
-const LazyComments = dynamic(() => import("@components/posts/comments/Comments"));
+export async function generateStaticParams() {
+    const posts = await getPosts();
+
+    return posts.map((post) => ({
+        id: post.id,
+    }));
+}
 
 export default async function PostPage({ params }) {
     const post = await getPost(params.id);
@@ -19,7 +25,7 @@ export default async function PostPage({ params }) {
             <hr />
             <div className={css.body}>{post.body}</div>
             <hr />
-            <LazyComments postId={params.id} />
+            <Comments postId={params.id} />
         </div>
     );
 }
